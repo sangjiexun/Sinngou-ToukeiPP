4cc95220672616d64ad6ad1723e7772ced4d4992
saiGtune <sai11121209@gmail.com>
2019-11-13 16:08:17 +0900

    未完成

--- 信号・統計PP/信号・統計PP/toukei_kadai8.py
+++ 信号・統計PP/信号・統計PP/toukei_kadai8.py
@@ -1,11 +1,13 @@
 # Library
 from numpy import *
 from matplotlib.pyplot import *
+from sklearn import *
 
 fileName = 'Data/weightHeightData.csv'
 sampleData = loadtxt(fileName,delimiter=',') 
 X = sampleData[:,(0,1)]
 y = sampleData[:,2]
+data = SVM(kernel='X')
 
 # Graph
 figure(figsize=(6,6)) 
@@ -14,3 +16,4 @@ plot(X[y==-1,0],X[y==-1,1],'rd')
 xlabel('Weight [kg]') 
 ylabel('Height [cm]')
 show()
+
