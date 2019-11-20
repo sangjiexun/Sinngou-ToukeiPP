e0eb3815b933036f75d6287037d515f1a2722afa
saiGtune <sai11121209@gmail.com>
2019-11-20 15:17:43 +0900

    sinngou_kadai9追加

--- 信号・統計PP/信号・統計PP/toukei_kadai8.py
+++ 信号・統計PP/信号・統計PP/toukei_kadai8.py
@@ -1,13 +1,16 @@
 # Library
 from numpy import *
 from matplotlib.pyplot import *
-from sklearn import *
+from sklearn.svm import SVC 
 
 fileName = 'Data/weightHeightData.csv'
 sampleData = loadtxt(fileName,delimiter=',') 
 X = sampleData[:,(0,1)]
 y = sampleData[:,2]
-data = SVM(kernel='X')
+svm = SVC(kernel='linear',C = 1.0)
+svm.fit(X[0],X[1])
+re = svc.predict(y)
+print(re)
 
 # Graph
 figure(figsize=(6,6)) 
@@ -15,5 +18,6 @@ plot(X[y==1,0],X[y==1,1],'bo')
 plot(X[y==-1,0],X[y==-1,1],'rd')
 xlabel('Weight [kg]') 
 ylabel('Height [cm]')
+grid()
 show()
 
