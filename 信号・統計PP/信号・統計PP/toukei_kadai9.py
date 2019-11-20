02e659b1079afc2900b25dbc8c95222bd2d75007
saichann <55883274+sai11121209@users.noreply.github.com>
2019-11-21 06:38:38 +0900

    完成v.1

--- 信号・統計PP/信号・統計PP/toukei_kadai9.py
+++ 信号・統計PP/信号・統計PP/toukei_kadai9.py
@@ -1,10 +1,10 @@
 #演習問題 9
 from numpy import *
-from matplotlib.pyplot import *
 from numpy.linalg import *
+from matplotlib.pyplot import *
 # データの読み込み
 fileName = 'Data/examResult2.csv'
-data = loadtxt(fileName, delimiter=',',skiprows=1,usecols=(1,2,3,4))
+data = loadtxt(fileName, delimiter=',',usecols=(1,2,3,4))
 X = data[:,(0,1)].T
 (M,K)=X.shape
 print(X)
@@ -12,10 +12,12 @@ print(X)
 C = cov(X)
 w,A = eig(C)
 index = argsort(w)
-rindex = index[range(M-1,-1,-1)]
+print(M)
+rindex = index[array(range(M-1,-1,-1))]
 w = w[rindex] # 降順に並べ替え
 A = A[:,rindex] # 降順に並べ替え
 Z = dot(A[:,0].T,X) # 第１主成分
-plot(Z,'.')
-show()
 print('Z=',Z)
+x = linspace(0,0,10)
+plot(Z,x,'.')
+show()
