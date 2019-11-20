e67eb2887e3082817acf71736ad860a7e59f4c45
saiGtune <sai11121209@gmail.com>
2019-11-20 15:25:35 +0900

    sinngou_kadai9

---
+++ 信号・統計PP/信号・統計PP/sinngou_kadai9.py
@@ -0,0 +1,16 @@
+
+from matplotlib.pyplot import *
+from numpy import *
+
+
+FigSize = (6,4)
+omega_c = 0.3
+omega=arange(0.01,1.0,0.01)
+H = 1/sqrt(1+(omega_c/omega)**2)
+fig=figure(figsize=FigSize)
+plot(omega, 20*log10(abs(H)))
+ylim(-10,2)
+grid(True)
+xlabel('Normalized Frequency')
+ylabel('Gain [dB]') 
+show()
